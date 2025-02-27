bda13c134d2861e197578be78cad78263210fe2c
PerryM123 <perrymiranda1@yahoo.com>
2025-02-27 10:12:25 +0900

    Added PostInfo model and migration tables

---
+++ src/database/migrations/2025_02_27_005032_create_post_infos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_info', function (Blueprint $table) {
+            $table->id('post_info_id');
+            $table->timestamps();
+            $table->unsignedBigInteger('ranking_categories_id');
+            $table->integer('rank_number');
+            $table->string('title');
+            $table->unsignedBigInteger('user_id');
+            $table->string('message_title');
+            $table->string('message_text');
+            // TODO: Post date needs to be changed for UTC time
+            $table->string('post_date');
+            $table->string('frame_type');
+            $table->boolean('is_edited');
+            $table->boolean('is_deleted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_info');
+    }
+}

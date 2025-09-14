38994f352f4db6f99932e4f337c91edee9162ec5
PerryM123 <perrymiranda1@yahoo.com>
2025-09-14 18:45:45 +0900

    レシートページネーションを返却するときにpage_countも返すように修正

--- src/app/Domain/ReceiptInfo/DTOs/PaginatedReceiptsDTO.php
+++ src/app/Domain/ReceiptInfo/DTOs/PaginatedReceiptsDTO.php
@@ -8,13 +8,15 @@ class PaginatedReceiptsDTO implements \JsonSerializable
 {
     public function __construct(
         private Collection $receipt_data,
-        private int $receipt_count
+        private int $receipt_count,
+        private int $page_count
     ) {}
     public function jsonSerialize(): array
     {
         return [
             'receipt_data' => $this->receipt_data,
             'receipt_count' => $this->receipt_count,
+            'page_count' => $this->page_count,
         ];
     }
 }
